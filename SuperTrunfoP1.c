83677347988e4226ce2ee4795235dda147f1b669
RobertaSouza-cmd <202503351198@alunos.estacio.br>
2025-03-07 19:09:25 -0300

    Add files via upload

---
+++ SuperTrunfoP1.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+int main () {
+
+    char Estado1, Estado2; 
+    //Declara a variavel Estado 
+
+    char CodigodaCarta1[50], CodigodaCarta2[50]; 
+    // Declara a variavel Codigo da Carta. O código da carta consiste em a letra que representa o estado e um número de zero a quatro 
+
+    char NomedaCidade1[50], NomedaCidade2[50]; 
+    //Declara a variavel Nome da Cidade
+
+    int Populacao1, Populacao2;
+    //Declara a variável População
+
+    float Area1, Area2;
+    //Declara a variável Área
+
+    float PIB1, PIB2;
+    //Declara a variável PIB 
+
+    int NumeroDePontosTuristicos1, NumeroDePontosTuristicos2;
+    //Declara a variável Número de Pontos Turisticos 
+
+    printf("Digite uma letra de A a H para representar o estado: \n ");
+    scanf(" %c", &Estado1);
+    //Pede para o usuário digitar uma letra de A a H para representar o estado e armazena na variavel estado 
+
+    printf("Digite o estado e um número de zero a quatro: \n");
+    scanf("%s", CodigodaCarta1);
+    printf("Você acabou de criar um código para o seu estado! \n");
+    //Pede para o usuário digitar o estado e um número de zero a quatro e armazena na variável Codigo da Carta
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", NomedaCidade1);
+    //Pede para o usuário digitar o nome da cidade e armazena na variável Nome da Cidade 
+
+    printf("Qual a população da cidade? \n");
+    scanf("%d", &Populacao1);
+    //Pede para o usuário digitar a população da cidade e armazena na variável População 
+
+    printf("Qual a área da cidade em km²? \n"); 
+    scanf("%f", &Area1); 
+    //Pede para o usuário digitar a área da cidade em km² e armazena na variável Área 
+
+    printf("Qual o PIB da cidade? \n");
+    scanf("%f", &PIB1);
+    //Pede para o usuário digitar o PIB da cidade e armazena na variável PIB
+
+    printf("Quantos pontos turísticos há na cidade? \n"); 
+    scanf("%d", &NumeroDePontosTuristicos1);
+    //Pede para o usuário digitar quantos pontos turísticos há na cidade e armazena na variável Número de Pontos Turísticos 
+
+
+    
+    printf("Estado: %c \n", Estado1);
+    printf("Código da Carta: %s \n", CodigodaCarta1);
+    printf("Nome da Cidade: %s \n", NomedaCidade1);
+    printf("População: %d \n", Populacao1);
+    printf("Área: %.2f km² \n", Area1); 
+    printf("PIB: %.5f \n", PIB1);
+    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos1); 
+
+    printf("Agora escolha sua segunda carta! \n");
+
+    printf("Digite uma letra de A a H para representar o estado: \n ");
+    scanf(" %c", &Estado2);
+
+    printf("Digite o estado e um número de zero a quatro: \n");
+    scanf("%s", CodigodaCarta2);
+    printf("Você acabou de criar um código para o seu estado! \n");
+    
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", NomedaCidade2);
+
+    printf("Qual a população da cidade? \n");
+    scanf("%d", &Populacao2);
+
+    printf("Qual a área da cidade em km²? \n"); 
+    scanf("%f", &Area2); 
+
+    printf("Qual o PIB da cidade? \n");
+    scanf("%f", &PIB2);
+
+    printf("Quantos pontos turísticos há na cidade? \n"); 
+    scanf("%d", &NumeroDePontosTuristicos2);
+
+
+    printf("Estado: %c \n", Estado2);
+    printf("Código da Carta: %s \n", CodigodaCarta2);
+    printf("Nome da Cidade: %s \n", NomedaCidade2);
+    printf("População: %d \n", Populacao2);
+    printf("Área: %.2f km² \n", Area2); 
+    printf("PIB: %.5f \n", PIB2);
+    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos2); 
+
+    return 0; 
+}
+
+
